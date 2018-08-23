069a4a22ae93d8da0e7b6f72ffc40be63c7bcf96
Marius Ngaboyamahina <ngabomarius@gmail.com>
2018-08-23 15:19:26 +0900

    First commit

---
+++ assets/AppAsset.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $sourcePath = '@app/html/sb-admin';
+    public $css = [
+        //'css/site.css',
+        'vendor/font-awesome/css/font-awesome.min.css',
+        'dist/css/sb-admin-2.css',
+    ];
+    public $js = [
+        'vendor/jquery/jquery.min.js',
+        'vendor/bootstrap/js/bootstrap.min.js',
+        'vendor/metisMenu/metisMenu.min.js',
+        'dist/js/sb-admin-2.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
---
+++ views/site/login.php
@@ -0,0 +1,38 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="row">
+    <div class="col-md-4 col-md-offset-4">
+        <div class="login-panel panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"><?php echo Yii::t('app', 'Please Sign In'); ?></h3>
+            </div>
+            <div class="panel-body">
+
+                <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+
+                <?= $form->field($model, 'username')
+                    ->textInput(['autofocus' => true, 'placeholder' => Yii::t('app', 'Enter email')])->label(false); ?>
+
+                <?= $form->field($model, 'password')
+                    ->passwordInput(['placeholder' => Yii::t('app', 'Enter password')])->label(false); ?>
+
+                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
+                <div class="form-group">
+                    <?= Html::submitButton(Yii::t('app', 'Login'), ['class' => 'btn btn-lg btn-success btn-block', 'name' => 'login-button']) ?>
+                </div>
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
+    </div>
+</div>
